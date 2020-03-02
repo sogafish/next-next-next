7dd727a87a6a3e762f7ee227465bf7ba0db12c3f
sogafish <k.sogafish@gmail.com>
2020-03-02 23:58:57 +0900

    link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
+import Link from 'next/link';
+
 const Index = () => (
+  <>
   <h1>pages/index.js</h1>
+  <ul>
+    <li>
+      <Link href="/h" as={`/h`}>
+        h
+      </Link>
+    </li>
+  </ul>
+  </>
 );
 
 export default Index;
